4f8fca99ff8aab4f8e3d75f99c379d2cb3f8879a
Nathan Silvera <nathan.s.untels@gmail.com>
2020-12-10 17:18:55 -0500

    Constraint Layout Terminado

---
+++ CL/app/src/main/java/com/dam/cl/MainActivity.java
@@ -0,0 +1,25 @@
+package com.dam.cl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotasActivity.class);
+                startActivity(i);
+    }
+        });
+    }
+}
